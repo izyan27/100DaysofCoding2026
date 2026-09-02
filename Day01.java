4b751c7b273a0546217386cd450d75309d6602bf
izyan27 <izmuhammad706@gmail.com>
2026-09-02 06:12:46 -0700

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,24 @@
+package Day01.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // Ini adalah komentar satu baris
+        
+        /* Ini adalah komentar multi-baris.
+   Semua teks di dalam area ini
+   akan diabaikan oleh compiler. */
+
+  /**
+ * Menghitung total harga setelah diskon.
+ * 
+ * @param harga   Harga awal barang
+ * @param diskon  Persentase diskon (0-100)
+ * @return        Harga akhir setelah dipotong diskon
+ */
+
+
+        
+    }
+    
+}
