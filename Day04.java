0490bb155e46bc405ff2535081cc184ffbb5e87d
izyan27 <izmuhammad706@gmail.com>
2026-09-05 08:15:06 -0700

    Create Day04.java

---
+++ Day04.java
@@ -0,0 +1,22 @@
+public class day005 {
+    public static void main(String[] args) {
+
+        // byte: 8 bit, untuk bilangan bulat kecil
+        byte semester = 2;
+
+        // short: 16 bit, untuk bilangan bulat yang lebih besar
+        short jumlahSKS = 24;
+
+        // int: 32 bit, untuk bilangan bulat yang sering digunakan
+        int uangSaku = 1500000;
+
+        // long: 64 bit, untuk angka yang sangat besar
+        long jumlahPendudukIndonesia = 280000000L;
+
+        // Menampilkan semua nilai ke layar
+        System.out.println("Semester: " + semester);
+        System.out.println("Jumlah SKS: " + jumlahSKS);
+        System.out.println("Uang Saku: Rp " + uangSaku);
+        System.out.println("Jumlah Penduduk Indonesia: " + jumlahPendudukIndonesia);
+    }
+}
