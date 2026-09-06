d90115fe43fbd0a03dd8f1faee307c7c68bf9571
izyan27 <izmuhammad706@gmail.com>
2026-09-06 04:54:20 -0700

    Create Day05.java

---
+++ Day05.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float tinggi = 170.5f;
+        double berat = 65.75;
+
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Berat: " + berat);
+    }
+}
